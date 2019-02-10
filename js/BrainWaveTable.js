2da134bb22f0b81d27af4fc208eb60a6b8b7c691
Matthew Horwell <mattjhorwell@gmail.com>
2019-02-10 11:33:41 +0000

    Added new table functionality

--- js/BrainWaveTable.js
+++ js/BrainWaveTable.js
@@ -2,7 +2,6 @@
 let request = new XMLHttpRequest();
 let output = document.getElementById("output");
 
-let surfData;
 let beachDBAPI = "http://localhost:8080/api/beach/";
 let searchString = document.getElementById("beachName").value;
 let currentLatitude = document.getElementById("latitude").value;
@@ -11,10 +10,11 @@ let maxDistance = document.getElementById("searchDistance").value;
 let distanceUnit = "N";
 let timeButtons = document.getElementById("timeButtons");
 let time = 0;
+let beachTable = document.getElementById("beachTable");
 
 function searchBeaches() {
 
-    document.getElementById("beachTable").className = "d-flex justify-content-around";
+    beachTable.className = "d-flex justify-content-around";
     document.getElementById("setTime").className = "d-flex justify-content-around";
 
     if (searchString != "") {
@@ -31,21 +31,22 @@ function searchBeaches() {
     request.responseType = "json";
     request.send();
     request.onload = function () {
-        let requestData = request.response;
+        let beachData = request.response;
 
-        for (let i = 0; i < requestData.length; i++) {
+        for (let i = 0; i < beachData.length; i++) {
 
-            let beachId = requestData[i].id;
-            let beachName = requestData[i].name;
-            let beachLatitude = requestData[i].latitude;
-            let beachLongtitude = requestData[i].longtitude;
+            let beachId = beachData[i].id;
+            let beachName = beachData[i].name;
+            let beachLatitude = beachData[i].latitude;
+            let beachLongtitude = beachData[i].longtitude;
             let row = output.insertRow();
             row.className = (beachId + beachName)
 
+            let idCell = row.insertCell();
             let nameCell = row.insertCell();
             let distanceCell = row.insertCell();
-            let minBreakCell = row.insertCell();
-            let runtimeCell = row.insertCell();
+            
+            
 
             nameCell.innerHTML = beachName;
             if (currentLatitude == 0 || currentLongtitude == 0) {
@@ -54,31 +55,37 @@ function searchBeaches() {
                 distanceCell.innerHTML = distance(currentLatitude, currentLongtitude,
                     beachLatitude, beachLongtitude, distanceUnit);
             }
-            let URL = "https://magicseaweed.com/api/196a716c7205dbe82df0d3c6377936e4/forecast/?spot_id=" +
-                969 +
-                "&fields=timestamp,swell.*"
-            getSurfData(URL);
-            console.log(surfData);
+
         }
+
     }
 }
 
-function getSurfData(URL) {
+
+function getSurfData() {
+    for (i = 0; i < timeButtons.length; i++) {
+        if (timeButtons[i].checked) {
+            time = timeButtons[i].value;
+            console.log("It's a match");
+            if (document.getElementById("PM").checked) {
+                time += 4;
+            }
+        }
+    }
+    let URL = "https://magicseaweed.com/api/196a716c7205dbe82df0d3c6377936e4/forecast/?spot_id=" +
+    969 +
+    "&fields=swell.*"
     PROXY = "https://cors-anywhere.herokuapp.com/";
     request.open("GET", PROXY + URL);
     request.responseType = "json";
     request.send();
     request.onload = function () {
-        for (i = 0; i < timeButtons.length; i++) {
-            if (timeButtons[i].checked) {
-                time = timeButtons[i].value;
-                console.log("It's a match");
-                if (document.getElementById("PM").checked) {
-                    time += 4;
-                }
-            }
+        let surfData = request.response[time];
+        let beachTable = document.getElementById("output");
+        for (var i = 0, row; row = beachTable.rows[i]; i++){
+            let minBreakCell = row.insertCell();
+            minBreakCell.innerHTML = surfData.swell.minBreakingHeight;
         }
-        surfData = request.response;
     }
 }
 
